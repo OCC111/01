897ff9a5e6e875959ea154ecaeea076073cf777f
OCC111 <15600686330@163.com>
2018-05-08 18:50:51 +0800

    hello

---
+++ 022.py
@@ -0,0 +1,21 @@
+a=6
+b=4
+c=2
+c=a+b
+print ("c-1的值:",c)
+c=a-b
+print ("c-2的值:",c)
+c=a*b 
+print ("c-3的值:",c)
+c=a/b
+print ("c-4的值:",c)
+a=5**2
+b=6**2
+c=a**b
+print ("c-5的值:",c)
+a=88
+b=44
+c=a//b
+print ("c-6的值:",c)
+
+# 01
